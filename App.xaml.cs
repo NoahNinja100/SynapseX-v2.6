1df0eb3044b61d002e8b3a7e6e2e161e98b49b7a
Noahninja100 <83727826+NoahNinja100@users.noreply.github.com>
2022-09-05 16:20:32 -0400

    SynapseXUI/

---
+++ App.xaml.cs
@@ -0,0 +1,124 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using ControlzEx.Theming;
+using sxlib.Specialized;
+using SynapseXUI.Entities;
+using SynapseXUI.UserControls;
+using SynapseXUI.Windows;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using static sxlib.Static.Data;
+
+namespace SynapseXUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static App Instance { get; private set; }
+        public static string StartupFolderPath { get; private set; }
+        public static string DataFolderPath { get; private set; }
+        public static string SettingsFilePath { get; private set; }
+        public static string TabsFilePath { get; private set; }
+        public static string EditorFilePath { get; private set; }
+        public static string ScriptsFolderPath { get; private set; }
+        public static SxLibWPF Lib { get; set; }
+        public static Options SxOptions { get; set; }
+        public static Settings Settings { get; private set; }
+        public static GitHub GitHub { get; private set; }
+        public static string VersionString { get; private set; }
+
+        public App()
+        {
+            CefSettings cefSettings = new CefSettings
+            {
+                LogSeverity = LogSeverity.Disable
+            };
+
+            if (!Cef.IsInitialized)
+            {
+                Cef.Initialize(cefSettings);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (Process.GetProcessesByName("SynapseXUI").ToList().Count > 1)
+            {
+                PromptWindow.Show("Synapse X UI", "An instance of this application is already running", PromptType.OK);
+                Environment.Exit(32);
+            }
+
+            Instance = this;
+            GitHub = new GitHub("casperb123", "SynapseXUI");
+            StartupFolderPath = Directory.GetCurrentDirectory();
+            ScriptsFolderPath = Path.Combine(StartupFolderPath, "scripts");
+            string libsFolderPath = Path.Combine(StartupFolderPath, "libs");
+            string aceFolderPath = Path.Combine(libsFolderPath, "ace");
+            DataFolderPath = Path.Combine(libsFolderPath, "data");
+            EditorFilePath = Path.Combine(aceFolderPath, "Editor.html");
+            SettingsFilePath = Path.Combine(DataFolderPath, "Options.ini");
+            TabsFilePath = Path.Combine(DataFolderPath, "Tabs.bin");
+            VersionString = Assembly.GetEntryAssembly().GetName().Version.ToString().Substring(0, 5);
+
+            if (!Directory.Exists(DataFolderPath))
+            {
+                Directory.CreateDirectory(DataFolderPath);
+            }
+
+            Settings = Settings.GetSettings(SettingsFilePath);
+            string theme = Settings.Theme.ApplicationTheme;
+            string color = Settings.Theme.ApplicationColor;
+            SetTheme(theme, color);
+            base.OnStartup(e);
+        }
+
+        public static void SetTheme(string theme, string color)
+        {
+            ThemeManager.Current.ChangeTheme(Current, $"{theme}.{color}");
+        }
+
+        public static void ResetSettings()
+        {
+            Settings.SetDefault();
+            EditorUserControl.Instance.columnScripts.Width = Settings.ScriptsListWidth;
+            Current.MainWindow.WindowState = Settings.WindowSize.WindowState;
+            Current.MainWindow.Width = Settings.WindowSize.WindowWidth;
+            Current.MainWindow.Height = Settings.WindowSize.WindowHeight;
+
+            Settings.Save(SettingsFilePath);
+        }
+
+        public static async Task CheckForUpdate(bool notifyIfLatest)
+        {
+            try
+            {
+                bool updateAvailable = await GitHub.CheckForUpdateAsync();
+                if (updateAvailable)
+                {
+                    if (PromptWindow.Show("Update Available", $"An update is available, would you like to download it?\n" +
+                                                              $"Current Version: {GitHub.CurrentVersion}\n" +
+                                                              $"Latest Version: {GitHub.LatestVersion}\n\n" +
+                                                              $"{GitHub.Changelog}", PromptType.YesNo))
+                    {
+                        GitHub.DownloadLatestRelease();
+                    }
+                }
+                else if (notifyIfLatest)
+                {
+                    PromptWindow.Show("No Update Available", "No update is available, you're already on the latest version", PromptType.OK);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MahApps.Metro.Controls;
+using SynapseXUI.Entities;
+using SynapseXUI.ViewModels;
+using SynapseXUI.Windows;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SynapseXUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public readonly MainWindowViewModel ViewModel;
+
+        public static MainWindow Instance { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            ViewModel = new MainWindowViewModel(this);
+            DataContext = ViewModel;
+        }
+
+        private void MetroWindow_Closing(object sender, CancelEventArgs e)
+        {
+            ViewModel.EditorUserControl.ViewModel.SaveTabs();
+            App.Settings.Save(App.SettingsFilePath);
+            Environment.Exit(0);
+        }
+
+        private void ButtonAttach_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Attach();
+        }
+
+        private void ButtonFlyoutTools_Click(object sender, RoutedEventArgs e)
+        {
+            flyoutTools.IsOpen = !flyoutTools.IsOpen;
+        }
+
+        private void ButtonExecuteSynapseHubScript_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.SelectedSynapseHubScript.Script.Execute();
+        }
+
+        private void ButtonKillRoblox_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.KillRoblox();
+        }
+
+        private void ButtonReinstallRoblox_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ReinstallRoblox();
+        }
+
+        private async void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            await App.CheckForUpdate(true);
+        }
+
+        private void IconGrid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            PromptWindow.Show("Synapse X", "This UI is developed by Casper:\n" +
+                                        "Discord: CasperTheGhost#3549", PromptType.OK);
+        }
+
+        private void MetroWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ViewModel.SetEditorScriptsMaxWidth();
+        }
+
+        private void MetroWindow_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                ViewModel.EditorUserControl.ViewModel.EndDragDrop(true);
+            }
+        }
+    }
+}
